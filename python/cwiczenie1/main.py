536bcebafbf37e679fdcb4e7e6230c0f5fd4fe54
JakubSkorupski <jskorupski_211i2@e.zseeim.edu.pl>
2024-03-15 11:06:40 +0000

    wyslanie pracy

---
+++ python/cwiczenie1/main.py
@@ -0,0 +1,9 @@
+x=10
+x=int(input("podaj liczbe"))
+if x>4:
+    print ("liczba jest wieksza od 4")
+z=40
+if x==10 and z==40:
+    print ("x rowna sie 10 a z rowna sie 40")
+print ("witaj swiecie")
+
