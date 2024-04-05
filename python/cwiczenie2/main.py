5203c240336b4173f0599ca3c1a8d2cdd710be59
JakubSkorupski <jskorupski_211i2@e.zseeim.edu.pl>
2024-04-05 10:19:51 +0000

    studentmachine send

---
+++ python/cwiczenie2/main.py
@@ -0,0 +1,12 @@
+licznik = 0
+odpowiedz=0
+while licznik < 3:
+    print("Jak masz na imię? ")
+    odpowiedz = input("Mam na imię ")
+
+    licznik += 1
+
+print("Dzień dobry", (odpowiedz))
+
+#    if odpowiedz.lower() != 'tak':
+#	break
